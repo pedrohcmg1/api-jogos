85832302c92632f667f4bdd1813c68410c0ef65d
pedrohcmg1 <pedromagalhaes2112@gmail.com>
2026-05-17 21:21:55 -0300

    Alteração do cors para a api funcionard

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
